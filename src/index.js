58a6dd60ddbd10bc42cdeb5e0edff44a1b50d95e
Sachin goyal <goyalsachin176@gmail.com>
2023-01-02 16:35:34 +0530

    basic cron scheduler

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express')
 
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service');
+// const { sendBasicEmail } = require('./services/email-service');
+
+const cron = require('node-cron')
 
 const setUpAndStartServer = () => {
     const app = express();
@@ -13,12 +15,18 @@ const setUpAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Server started on the port ${PORT}`);
 
-        sendBasicEmail(
-            'goyalsachin171@gmail.com',
-            'goyalsachin176@gmail.com',
-            'This is a testing mail',
-            'Hey , how are you? I hope you like the support'
-        )
+        //sending mail through nodemailer below
+        // sendBasicEmail(
+        //     'goyalsachin171@gmail.com',
+        //     'goyalsachin176@gmail.com',
+        //     'This is a testing mail',
+        //     'Hey , how are you? I hope you like the support'
+        // )
+
+        //this cron job does the task every 2 minutes, we can use it to schedule the job any time just reading the documentation
+        // cron.schedule('*/2 * * * *', () => {
+        //     console.log('running a task every two minutes')
+        // })
     })
     
 }
